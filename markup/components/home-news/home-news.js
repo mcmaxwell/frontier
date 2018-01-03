68996b851804d5c322e1b5912256acc35b51e0c4
Maxim Liutsko <mcmaxwell07@gmail.com>
2018-01-03 14:33:30 +0200

    add news page

--- markup/components/home-news/home-news.js
+++ markup/components/home-news/home-news.js
@@ -2,26 +2,25 @@ const $slickSlider = $('.js-news-grid')
 const settings = {
   dots: true,
   arrows: false,
-  variableWidth: true,
   infinite: false
 }
 
-if ($(window).width() < 768) {
-  $('.js-news-grid').after($('.all-news'))
-  $slickSlider.slick(settings)
-}
-
-// reslick only if it's not slick()
-$(window).on('resize', function () {
-  if ($(window).width() > 768) {
+function initSlider () {
+  if ($(window).width() < 768) {
+    $('.js-news-grid').after($('.all-news'))
+    if (!$slickSlider.hasClass('slick-initialized')) {
+      $slickSlider.slick(settings)
+    }
+  } else {
     if ($slickSlider.hasClass('slick-initialized')) {
       $slickSlider.slick('unslick')
     }
-    return
+    $('.all-news').appendTo($('.js-news-grid'))
   }
+}
 
-  if (!$slickSlider.hasClass('slick-initialized')) {
-    $('.js-news-grid .all-news').remove()
-    return $slickSlider.slick(settings)
-  }
+initSlider()
+
+$(window).on('resize', function () {
+  initSlider()
 })
---
+++ markup/components/news-item/data/data.js
@@ -0,0 +1,10 @@
+newsItem: {
+  no: {
+    title: 'Opprinnelses sertifikat Din garanti for «grønn» energi',
+    text: `
+    <p>All the rumors have finally died down and many skeptics have tightened their lips, the iPod does support video format now on its fifth generation. While the iPod is not the first to come up with this, it has certainly made its stature as the greatest in the market and can be dubbed as the best multimedia portable player available.</p>
+    <img src="./static/img/assets/news-item/news1.jpg">
+    <p>With its popularity and iconesque standing, the iPod has made sharing videos easier. You don’t need to be lugging around different equipments for your music, your notes, your photos and your videos. You can have the standard where every other portable multimedia players are gauged to and not need anything else. Video playback capabilities have made it a complete multi media equipment, but not all formats are supported just yet. So if you want to put your videos in your iPod you have to make sure that they are in a supported format. But don’t worry, you can convert your videos into the supported format so you can still upload and watch it in your iPod video. All it takes is a special software that is programmed to do the conversion act. This is a program generally dubbed as a video converter.</p>
+    `
+  }
+}

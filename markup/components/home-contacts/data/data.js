1dd24ef5013ebe340f20391d9e57787509f81693
Maxim Liutsko <mcmaxwell07@gmail.com>
2018-01-12 10:07:12 +0200

    fix

--- markup/components/home-contacts/data/data.js
+++ markup/components/home-contacts/data/data.js
@@ -9,7 +9,7 @@ homeContacts: {
         name: 'Roman Fredriksen',
         position: 'Kundekontakt',
         tel: '+47 480 69 506',
-        email: 'roman@probarents.no',
+        email: 'roman@frontiernorway.com',
         linkedinName: 'romanfredriksen',
         linkedinLink: 'https://www.linkedin.com/in/roman-fredriksen-603346a5/'
       },
@@ -19,7 +19,7 @@ homeContacts: {
         name: 'Fred Falch',
         position: 'Styremedlem',
         tel: '+47 476 49 000',
-        email: 'fred.falch@icloud.com',
+        email: 'fred@frontiernorway.com',
         linkedinName: 'fredfalch',
         linkedinLink: 'https://www.linkedin.com/in/fred-falch-a6794a/'
       },
@@ -29,7 +29,7 @@ homeContacts: {
         name: 'Morten Guttormsen',
         position: 'Styreleder',
         tel: '+47 917 13 361',
-        email: 'morten.guttormsen@sinus247.no',
+        email: 'morten@frontiernorway.com',
         linkedinName: 'mortenguttormsen',
         linkedinLink: 'https://www.linkedin.com/in/morten-guttormsen-82b5b124/'
       },
@@ -39,7 +39,7 @@ homeContacts: {
         name: 'Ronny Sandstrak',
         position: 'Installasjon',
         tel: '+47 950 57 490',
-        email: 'ronny.sandstrak@sinus247.no',
+        email: 'ronny@frontiernorway.com',
         linkedinName: 'fredfalch',
         linkedinLink: 'https://www.linkedin.com/in/ronny-sandstrak-899aa9b4/'
       },

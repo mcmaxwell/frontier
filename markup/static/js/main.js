2e6b0213b62df6b257cba2b61925ad848c228cda
Maxim Liutsko <mcmaxwell07@gmail.com>
2018-01-12 10:18:56 +0200

    fix map

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
   /* Every time the window is scrolled ... */
   function showElements () {
     $('.hideme').each( function () {
-      let bottomObject = $(this).position().top + 100
+      let bottomObject = $(this).position().top + 50
       let bottomWindow = $(window).scrollTop() + $(window).height()
 
       /* If the object is completely visible in the window, fade it it */

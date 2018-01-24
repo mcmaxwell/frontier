8b4b0862f2d746b132ce65f46ebc8177f346671a
Maxim Liutsko <mcmaxwell07@gmail.com>
2018-01-24 22:23:56 +0200

    fix

--- markup/components/home-top/data/data.js
+++ markup/components/home-top/data/data.js
@@ -2,6 +2,13 @@ homeTop: {
   no: {
     image: '%=static=%img/assets/home-top/top-image.jpg',
     title: 'Vi leverer en enkel og miljøvennlig løsning for effektiv drift',
-    desc: 'Din leverandør av elektrisk- og fiber anlegg til havbruk'
-  }
+    desc: 'Din leverandør av elektrisk- og fiber anlegg til havbruk',
+    scroll: 'Bla Nedover'
+  },
+  no: {
+    image: '%=static=%img/assets/home-top/top-image.jpg',
+    title: 'Vi leverer en enkel og miljøvennlig løsning for effektiv drift',
+    desc: 'Din leverandør av elektrisk- og fiber anlegg til havbruk',
+    scroll: 'Scroll Down'
+  },
 }

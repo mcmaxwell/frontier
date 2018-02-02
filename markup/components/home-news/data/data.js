6898ad25eba5130e41ba9271bd1675b8e008e1a9
Maxim Liutsko <mcmaxwell07@gmail.com>
2018-02-02 17:03:47 +0200

    fix

--- markup/components/home-news/data/data.js
+++ markup/components/home-news/data/data.js
@@ -9,24 +9,12 @@ homeNews: {
         desc: 'Frontier Infrastructure utsteder opprinnelses garantier av din strømleveranse i samarbeid med Ishavskraft.',
         link: 'news-item.html'
       },
-      {
-        date: 'Oct 18, 2017',
-        title: 'Understanding Operating Systems',
-        desc: 'If you own an Iphone, you’ve probably already worked out how much fun it is to use it to watch movies-it  movies-it  movies-it has that nice…',
-        link: 'news-item.html'
-      },
-      {
-        date: 'Oct 18, 2017',
-        title: 'Vi leverer en enkel og miljøvennlig løsning for effektiv drift',
-        desc: 'If you own an Iphone, you’ve probably already worked out how much fun it is to use it to watch movies-it  movies-it  movies-it has that nice…',
-        link: 'news-item.html'
-      },
-      {
-        date: 'Oct 18, 2017',
-        title: 'Understanding Operating Systems',
-        desc: 'Vi kan dermed tilby 100 % fornybar energi til alle selskapets kunder',
-        link: 'news-item.html'
-      },
+      // {
+      //   date: 'Oct 18, 2017',
+      //   title: 'Understanding Operating Systems',
+      //   desc: 'If you own an Iphone, you’ve probably already worked out how much fun it is to use it to watch movies-it  movies-it  movies-it has that nice…',
+      //   link: 'news-item.html'
+      // }
     ]
   }
 }
--- markup/components/home-news/home-news.js
+++ markup/components/home-news/home-news.js
@@ -1,26 +1,26 @@
-const $slickSlider = $('.js-news-grid')
-const settings = {
-  dots: true,
-  arrows: false,
-  infinite: false
-}
-
-function initSlider () {
-  if ($(window).width() < 768) {
-    $('.js-news-grid').after($('.all-news'))
-    if (!$slickSlider.hasClass('slick-initialized')) {
-      $slickSlider.slick(settings)
-    }
-  } else {
-    if ($slickSlider.hasClass('slick-initialized')) {
-      $slickSlider.slick('unslick')
-    }
-    $('.all-news').appendTo($('.js-news-grid'))
-  }
-}
-
-initSlider()
-
-$(window).on('resize', function () {
-  initSlider()
-})
+// const $slickSlider = $('.js-news-grid')
+// const settings = {
+//   dots: true,
+//   arrows: false,
+//   infinite: false
+// }
+//
+// function initSlider () {
+//   if ($(window).width() < 768) {
+//     $('.js-news-grid').after($('.all-news'))
+//     if (!$slickSlider.hasClass('slick-initialized')) {
+//       $slickSlider.slick(settings)
+//     }
+//   } else {
+//     if ($slickSlider.hasClass('slick-initialized')) {
+//       $slickSlider.slick('unslick')
+//     }
+//     $('.all-news').appendTo($('.js-news-grid'))
+//   }
+// }
+//
+// initSlider()
+//
+// $(window).on('resize', function () {
+//   initSlider()
+// })

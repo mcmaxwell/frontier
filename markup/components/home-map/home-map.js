5c27d30f59eea977c539eda9cd13f2d2b06a7ee0
Maxim Liutsko <mcmaxwell07@gmail.com>
2018-02-05 15:10:42 +0200

    fix

--- markup/components/home-map/home-map.js
+++ markup/components/home-map/home-map.js
@@ -6,26 +6,34 @@ $('.home-map__toggle').click(function () {
   return false
 })
 
-$('.home-map__item').click(function () {
-  $('.home-map__item-list').removeClass('active')
-  $('.home-map__item').removeClass('active')
-  if($(this).hasClass('active')) {
-    $(this).removeClass('active')
-    $(this).next().removeClass('active')
-  }
-  else {
-    $(this).addClass('active')
-    $(this).next().toggleClass('active')
-  }
+$('.home-map__item').click(function (e) {
 
-  return false
+  // $('.home-map__item-list').removeClass('active')
+  // $('.home-map__item').removeClass('active')
+  //
+  // if($(this).hasClass('active')) {
+  //   $(this).removeClass('active')
+  //   $(this).next().removeClass('active')
+  // }
+  // else {
+  //   $(this).addClass('active')
+  //   $(this).next().toggleClass('active')
+  // }
+
+  return false;
 })
 
 $(document).mouseup(function (e){ // событие клика по веб-документу
-  var div = $('.home-map__item-list'); // тут указываем ID элемента
-  if (!div.is(e.target) // если клик был не по нашему блоку
-      && div.has(e.target).length === 0) { // и не по его дочерним элементам
-    div.removeClass('active'); // скрываем его
+  var list = $('.home-map__item-list'); // тут указываем ID элемента
+  var item = $('.home-map__item');
+
+  if (item.is(e.target) && !$(e.target).hasClass('active')  ) {
+    $('.home-map__item-list').removeClass('active')
+    $('.home-map__item').removeClass('active')
+    $(e.target).toggleClass('active')
+     $(e.target).next().toggleClass('active')
+  } else if (!list.is(e.target) && list.has(e.target).length === 0) { // и не по его дочерним элементам
+    list.removeClass('active'); // скрываем его
     $('.home-map__item').removeClass('active');
   }
 });
